a8f2cdd72df7dd1786e3d39e195558f2a92a4167
Basit Ayantunde <rlamarrr@gmail.com>
2020-05-31 15:57:23 +0000

    added panic hook example

--- examples/panic_hook/main.cc
+++ examples/panic_hook/main.cc
@@ -1,3 +1,18 @@
-#include "stx/panic.h"
+#include <iostream>
 
-int main() { stx::panic("crash and burn!"); }
+#include "stx/panic.h"
+#include "stx/panic/hook.h"
+
+void hook(std::string_view info, stx::ReportPayload const& payload,
+          stx::SourceLocation location) noexcept {
+  std::cout << "========= panic from custom hook =========\n";
+  std::cout << info << '\n';
+  std::cout << payload.data() << '\n';
+}
+
+int main() {
+  if (!stx::attach_panic_hook(&hook)) {
+    std::cout << "unable to set panic hook" << std::endl;
+  };
+  stx::panic("crash and burn!");
+}
