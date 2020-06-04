e5ba87e5a818c7e631e92c2fb069809be719381b
Lamar <rlamarrr@gmail.com>
2020-06-04 22:15:38 +0100

    changed to c++ 17

--- include/stx/panic.h
+++ include/stx/panic.h
@@ -12,20 +12,9 @@
 #pragma once
 
 #include "stx/report.h"
-
-#ifdef STX_STABLE_LIB_SOURCE_LOCATION
-#include <source_location>
-#else
-#include <experimental/source_location>
-#endif
+#include "stx/source_location.h"
 
 namespace stx {
-
-#ifdef STX_STABLE_SOURCE_LOCATION
-using SourceLocation = std::source_location;
-#else
-using SourceLocation = std::experimental::source_location;
-#endif
 
 // here, we can avoid any form of memory allocation that might be needed,
 // therefore deferring the info string and report payload to the callee and can
@@ -47,23 +36,19 @@ STX_LOCAL void panic_handler(std::string_view info,
 /// in example code and in tests. `panic` is closely tied with the `unwrap` and
 /// `expect` method of both `Option` and `Result`. Both implementations call
 /// `panic` when they are set to `None` or `Err` variants.
-[[noreturn]] STX_FORCE_INLINE void panic(
-    std::string_view info,
-    SourceLocation location = SourceLocation::current()) noexcept {
-  begin_panic(std::move(info), ReportPayload(Report("")), std::move(location));
-}
 
+template <typename T>
 [[noreturn]] STX_FORCE_INLINE void panic(
-    std::string_view info, Reportable auto const& value,
+    std::string_view info, T const& value,
     SourceLocation location = SourceLocation::current()) noexcept {
   begin_panic(std::move(info), ReportPayload(internal::report::query >> value),
               std::move(location));
 }
 
+template <typename T = void>
 [[noreturn]] STX_FORCE_INLINE void panic(
-    std::string_view info, auto const& value,
+    std::string_view info,
     SourceLocation location = SourceLocation::current()) noexcept {
-  (void)value;
   begin_panic(std::move(info), ReportPayload(Report("")), std::move(location));
 }
 
