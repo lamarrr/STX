fb021dce8f141f5640c65cc7e66cabb4111af30a
Basit Ayantunde <rlamarrr@gmail.com>
2020-06-05 20:53:23 +0000

    added license to source files

--- include/stx/internal/try.h
+++ include/stx/internal/try.h
@@ -1,8 +1,8 @@
 /**
- * @file halt.h
+ * @file option_result.h
  * @author Basit Ayantunde <rlamarrr@gmail.com>
  * @version  0.0.1
- * @date 2020-04-26
+ * @date 2020-06-05
  *
  * @copyright MIT License
  *
@@ -28,25 +28,9 @@
  *
  */
 
-#pragma once
+/// @file
+///
+/// Error Propagation Macros
+///
+///
 
-#include "stx/panic.h"
-
-namespace stx {
-
-/// Causes the program, or the current thread, to halt by entering an infinite
-/// loop.
-/// You can force the program to continue via a debugger by setting the
-/// `proceed` value to true.
-inline void panic_halt(
-    std::string_view info, ReportPayload const& payload,
-    SourceLocation location = SourceLocation::current()) noexcept {
-  (void)info;
-  (void)payload;
-  (void)location;
-
-  volatile bool const halt = true;
-  while (halt) {
-  }
-}
-};  // namespace stx
