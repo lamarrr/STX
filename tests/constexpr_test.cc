8b9f60d6b33589b5eb725485c1fd4298f8c170a8
Basit Ayantunde <rlamarrr@gmail.com>
2020-06-08 21:31:35 +0000

    added constexpr tests

--- tests/constexpr_test.cc
+++ tests/constexpr_test.cc
@@ -31,7 +31,7 @@
 #include "stx/option.h"
 #include "stx/result.h"
 
-#if STX_OPTION_CONSTEXPR || STX_RESULT_CONSTEXPR
+#if STX_OPTION_CONSTEXPR && STX_RESULT_CONSTEXPR  // check if it'll work
 
 using stx::Ok, stx::Err, stx::Result;
 using stx::Some, stx::None, stx::Option;
@@ -41,6 +41,29 @@ constexpr auto divide(int x, int y) -> Option<int> {
   return Some(x / y);
 }
 
-constexpr auto divide_result = divide(1, 56);
+static_assert(divide(56, 1).unwrap_or_default() == 56);
+static_assert(divide(56, 0).unwrap_or_default() == 0);
+
+static_assert(divide(56, 10).match([](int v) { return v; },
+                                   []() { return -1; }) == 5);
+static_assert(divide(56, 0).match([](int v) { return v; },
+                                  []() { return -1; }) == -1);
+
+enum class Error { Range };
+
+constexpr auto add(int8_t x, int8_t y) -> Result<int8_t, Error> {
+  int16_t acc = x;
+  acc += y;
+
+  if (acc > 127) return Err(Error::Range);
+  return Ok((int8_t)acc);
+}
+
+static_assert(add(0, 10).unwrap_or_default() == 10);
+static_assert(add(127, 1).unwrap_or_default() == 0);
+static_assert(add(56, 10).match([](int8_t v) { return v; },
+                                [](Error) { return -1; }) == 66);
+static_assert(add(56, 100).match([](int8_t v) { return v; },
+                                 [](Error) { return -1; }) == -1);
 
 #endif
