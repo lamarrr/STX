ea579d882493d949992519250f8cedbd808e0a5b
Basit Ayantunde <rlamarrr@gmail.com>
2020-06-12 16:04:37 +0000

    improved panic logging macros

--- include/stx/panic/handlers/print.h
+++ include/stx/panic/handlers/print.h
@@ -36,18 +36,18 @@
 /// Safe snprintfs
 
 /// printf using a provided buffer
-#define STX_PANIC_EPRINTF_WITH(STX_ARG_BUFFER, STX_ARG_BUFFER_SIZE,   \
-                               STX_ARG_FORMAT, STX_ARG_VALUE)         \
-  {                                                                   \
-    int fmt_size = std::snprintf(STX_ARG_BUFFER, STX_ARG_BUFFER_SIZE, \
-                                 STX_ARG_FORMAT, STX_ARG_VALUE);      \
-    if (fmt_size >= STX_ARG_BUFFER_SIZE) {                            \
-      std::fputs("<format buffer insufficient>", stderr);             \
-    } else if (fmt_size < 0) {                                        \
-      std::fputs("<format implementation error>", stderr);            \
-    } else {                                                          \
-      std::fputs(STX_ARG_BUFFER, stderr);                             \
-    };                                                                \
+#define STX_PANIC_EPRINTF_WITH(STX_ARG_BUFFER, STX_ARG_BUFFER_SIZE,     \
+                               STX_ARG_FORMAT, STX_ARG_VALUE)           \
+  {                                                                     \
+    int fmt_size = ::std::snprintf(STX_ARG_BUFFER, STX_ARG_BUFFER_SIZE, \
+                                   STX_ARG_FORMAT, STX_ARG_VALUE);      \
+    if (fmt_size >= STX_ARG_BUFFER_SIZE) {                              \
+      ::std::fputs("<format buffer insufficient>", stderr);             \
+    } else if (fmt_size < 0) {                                          \
+      ::std::fputs("<format implementation error>", stderr);            \
+    } else {                                                            \
+      ::std::fputs(STX_ARG_BUFFER, stderr);                             \
+    };                                                                  \
   }
 
 /// printf, makes own buffer
@@ -55,13 +55,13 @@
   {                                                                        \
     /* string length + terminating null character */                       \
     char fmt_buffer[STX_ARG_STR_SIZE + 1];                                 \
-    int fmt_size = std::snprintf(fmt_buffer, STX_ARG_STR_SIZE + 1,         \
-                                 STX_ARG_FORMAT, STX_ARG_VALUE);           \
+    int fmt_size = ::std::snprintf(fmt_buffer, STX_ARG_STR_SIZE + 1,       \
+                                   STX_ARG_FORMAT, STX_ARG_VALUE);         \
     if (fmt_size >= STX_ARG_STR_SIZE) {                                    \
-      std::fputs("<format buffer insufficient>", stderr);                  \
+      ::std::fputs("<format buffer insufficient>", stderr);                \
     } else if (fmt_size < 0) {                                             \
-      std::fputs("<format implementation error>", stderr);                 \
+      ::std::fputs("<format implementation error>", stderr);               \
     } else {                                                               \
-      std::fputs(fmt_buffer, stderr);                                      \
+      ::std::fputs(fmt_buffer, stderr);                                    \
     };                                                                     \
   }
