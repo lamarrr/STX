cc2fba5a2ca27054dbfaca1377596ec879c82ab1
Basit Ayantunde <rlamarrr@gmail.com>
2021-08-02 17:51:20 +0100

    added tests to project setup cmake macro

--- cmake-modules/add_project_library.cmake
+++ cmake-modules/add_project_library.cmake
@@ -15,9 +15,27 @@ macro(add_project_library target_project_name target_library_name)
 
   add_library(${library_name} STATIC ${${library_name}_SOURCE_FILES})
   add_library(${library_alias} ALIAS ${library_name})
+  set_target_properties(
+    ${library_name} PROPERTIES CXX_STANDARD
+                               ${${target_project_name}_CXX_STANDARD})
 
   target_include_directories(${library_name}
                              PUBLIC ${${library_name}_PUBLIC_INCLUDE_DIRECTORY})
   target_include_directories(
     ${library_name} PRIVATE ${${library_name}_PRIVATE_INCLUDE_DIRECTORY})
+
+  string(TOUPPER ${target_project_name} target_project_name_upper)
+
+  file(GLOB ${library_name}_TESTS_SOURCE_FILES_LIST tests/*.cc)
+
+  if(${${target_project_name_upper}_BUILD_TESTS})
+    if(${library_name}_TESTS_SOURCE_FILES_LIST)
+      add_executable(${library_name}_tests
+                     ${${library_name}_TESTS_SOURCE_FILES_LIST})
+      target_include_directories(${library_name}_tests PRIVATE tests)
+      target_link_libraries(${library_name}_tests gtest_main gtest
+                            ${library_alias})
+    endif()
+  endif()
+
 endmacro()
