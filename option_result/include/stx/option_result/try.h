394f32d12a495d1d879623ecea07d95c7ebf9f60
Basit Ayantunde <rlamarrr@gmail.com>
2021-08-02 22:41:03 +0100

    prevented macro from swallowing commas in try macros

--- option_result/include/stx/option_result/try.h
+++ option_result/include/stx/option_result/try.h
@@ -46,41 +46,41 @@ STX_END_NAMESPACE
 #define STX_WITH_UNIQUE_SUFFIX_(x, y) STX_TRY__UTIL_JOIN_(x, y)
 
 #define STX_TRY_OK_IMPL_(STX_ARG_UNIQUE_PLACEHOLDER, qualifier_identifier,     \
-                         result_expr)                                          \
-  static_assert(!::std::is_const_v<decltype((result_expr))>,                   \
-                "the expression: ' " #result_expr                              \
+                         ...)                                                  \
+  static_assert(!::std::is_const_v<decltype((__VA_ARGS__))>,                   \
+                "the expression: ' " #__VA_ARGS__                              \
                 " ' evaluates to a const and is not mutable");                 \
   static_assert(                                                               \
-      !::std::is_lvalue_reference_v<decltype((result_expr))>,                  \
-      "the expression: ' " #result_expr                                        \
+      !::std::is_lvalue_reference_v<decltype((__VA_ARGS__))>,                  \
+      "the expression: ' " #__VA_ARGS__                                        \
       " ' evaluates to an l-value reference, 'TRY_OK' only accepts r-values "  \
       "and r-value references ");                                              \
-  decltype((result_expr))&& STX_ARG_UNIQUE_PLACEHOLDER = (result_expr);        \
+  decltype((__VA_ARGS__))&& STX_ARG_UNIQUE_PLACEHOLDER = (__VA_ARGS__);        \
                                                                                \
   if (STX_ARG_UNIQUE_PLACEHOLDER.is_err())                                     \
     return ::stx::Err<typename std::remove_reference_t<decltype(               \
-        (result_expr))>::error_type>(                                          \
+        (__VA_ARGS__))>::error_type>(                                          \
         ::stx::internal::result::unsafe_err_move(STX_ARG_UNIQUE_PLACEHOLDER)); \
                                                                                \
-  typename std::remove_reference_t<decltype((result_expr))>::value_type        \
+  typename std::remove_reference_t<decltype((__VA_ARGS__))>::value_type        \
       qualifier_identifier = ::stx::internal::result::unsafe_value_move(       \
           STX_ARG_UNIQUE_PLACEHOLDER);
 
 #define STX_TRY_SOME_IMPL_(STX_ARG_UNIQUE_PLACEHOLDER, qualifier_identifier, \
-                           option_expr)                                      \
-  static_assert(!::std::is_const_v<decltype((option_expr))>,                 \
-                "the expression: ' " #option_expr                            \
+                           ...)                                              \
+  static_assert(!::std::is_const_v<decltype((__VA_ARGS__))>,                 \
+                "the expression: ' " #__VA_ARGS__                            \
                 " ' evaluates to a const and is not mutable");               \
-  static_assert(!::std::is_lvalue_reference_v<decltype((option_expr))>,      \
-                "the expression: ' " #option_expr                            \
+  static_assert(!::std::is_lvalue_reference_v<decltype((__VA_ARGS__))>,      \
+                "the expression: ' " #__VA_ARGS__                            \
                 " ' evaluates to an l-value reference, 'TRY_SOME' only "     \
                 "accepts r-values "                                          \
                 "and r-value references ");                                  \
-  decltype((option_expr))&& STX_ARG_UNIQUE_PLACEHOLDER = (option_expr);      \
+  decltype((__VA_ARGS__))&& STX_ARG_UNIQUE_PLACEHOLDER = (__VA_ARGS__);      \
                                                                              \
   if (STX_ARG_UNIQUE_PLACEHOLDER.is_none()) return ::stx::None;              \
                                                                              \
-  typename std::remove_reference_t<decltype((option_expr))>::value_type      \
+  typename std::remove_reference_t<decltype((__VA_ARGS__))>::value_type      \
       qualifier_identifier = ::stx::internal::option::unsafe_value_move(     \
           STX_ARG_UNIQUE_PLACEHOLDER);
 
@@ -89,17 +89,17 @@ STX_END_NAMESPACE
 ///
 /// `result_expr` must be an expression yielding an r-value (reference) of type
 /// `Result`
-#define TRY_OK(qualifier_identifier, result_expr)                   \
+#define TRY_OK(qualifier_identifier, ...)                           \
   STX_TRY_OK_IMPL_(                                                 \
       STX_WITH_UNIQUE_SUFFIX_(STX_TRY_OK_PLACEHOLDER, __COUNTER__), \
-      qualifier_identifier, result_expr)
+      qualifier_identifier, __VA_ARGS__)
 
 /// if `option_expr` evaluates to an `Option` containing a `None`, `TRY_SOME`
 /// returns its `None` value.
 ///
 /// `option_expr` must be an expression yielding an r-value (reference) of type
 /// `Option`
-#define TRY_SOME(qualifier_identifier, option_expr)                   \
+#define TRY_SOME(qualifier_identifier, ...)                           \
   STX_TRY_SOME_IMPL_(                                                 \
       STX_WITH_UNIQUE_SUFFIX_(STX_TRY_SOME_PLACEHOLDER, __COUNTER__), \
-      qualifier_identifier, option_expr)
+      qualifier_identifier, __VA_ARGS__)
