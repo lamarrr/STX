d585c8b18411a25028f84948219fb48c03d820d4
Basit Ayantunde <rlamarrr@gmail.com>
2022-07-16 10:49:03 +0100

    updated code

--- option_result/tests/report_test.cc
+++ option_result/tests/report_test.cc
@@ -52,92 +52,73 @@ std::string_view operator>>(ReportQuery, IoError const& v) {
 static constexpr auto query = ReportQuery{};
 
 TEST(ReportTest, FormatPointer) {
-  EXPECT_TRUE((query >> (int*)0x28e7).what() == "0x28e7"s);
+  EXPECT_TRUE((query >> (int*)0x28e7) == "0x28e7"s);
 
-  EXPECT_TRUE((query >> (int const*)0x28e7).what() == "0x28e7"s);
+  EXPECT_TRUE((query >> (int const*)0x28e7) == "0x28e7"s);
 
   int const* const c = (int const*)0x28e7;
-  EXPECT_TRUE((query >> c).what() == "0x28e7"s);
+  EXPECT_TRUE((query >> c) == "0x28e7"s);
 }
 
 TEST(ReportTest, FormatUInt8) {
   uint8_t a = 255;
-  EXPECT_EQ((query >> a).what(), "255");
+  EXPECT_EQ(query >> a, "255");
 
   uint8_t b = 0;
-  EXPECT_EQ((query >> b).what(), "0");
+  EXPECT_EQ(query >> b, "0");
 }
 
 TEST(ReportTest, FormatInt8) {
   int8_t a = 127;
-  EXPECT_EQ((query >> a).what(), "127");
+  EXPECT_EQ(query >> a, "127");
 
   int8_t b = -128;
-  EXPECT_EQ((query >> b).what(), "-128");
+  EXPECT_EQ(query >> b, "-128");
 }
 
 TEST(ReportTest, FormatUInt16) {
   uint16_t a = std::numeric_limits<uint16_t>::min();
-  EXPECT_EQ((query >> a).what(), to_string(a));
+  EXPECT_EQ(query >> a, to_string(a));
 
   uint16_t b = std::numeric_limits<uint16_t>::max();
-  EXPECT_EQ((query >> b).what(), to_string(b));
+  EXPECT_EQ(query >> b, to_string(b));
 }
 
 TEST(ReportTest, FormatInt16) {
   int16_t a = std::numeric_limits<int16_t>::min();
-  EXPECT_EQ((query >> a).what(), to_string(a));
+  EXPECT_EQ(query >> a, to_string(a));
 
   int16_t b = std::numeric_limits<int16_t>::max();
-  EXPECT_EQ((query >> b).what(), to_string(b));
+  EXPECT_EQ(query >> b, to_string(b));
 }
 
 TEST(ReportTest, FormatUInt32) {
   uint32_t a = std::numeric_limits<uint32_t>::min();
-  EXPECT_EQ((query >> a).what(), to_string(a));
+  EXPECT_EQ(query >> a, to_string(a));
 
   uint32_t b = std::numeric_limits<uint32_t>::max();
-  EXPECT_EQ((query >> b).what(), to_string(b));
+  EXPECT_EQ(query >> b, to_string(b));
 }
 
 TEST(ReportTest, FormatInt32) {
   int32_t a = std::numeric_limits<int32_t>::min();
-  EXPECT_EQ((query >> a).what(), to_string(a));
+  EXPECT_EQ(query >> a, to_string(a));
 
   int32_t b = std::numeric_limits<int32_t>::max();
-  EXPECT_EQ((query >> b).what(), to_string(b));
+  EXPECT_EQ(query >> b, to_string(b));
 }
 
 TEST(ReportTest, FormatEnum) {
   using namespace std::string_view_literals;
 
-  EXPECT_EQ((query >> IoError::EoF).what(), "End of File");
+  EXPECT_EQ(query >> IoError::EoF, "End of File");
 
-  EXPECT_EQ((query >> IoError::__Reserved).what(), "Unknown Error");
-
-  auto a = std::string();
-  for (size_t i = 0; i < kReportReserveSize; i++) {
-    a += "H";
-  }
-
-  auto b = std::string();
-  for (size_t i = 0; i < kReportReserveSize + 1; i++) {
-    b += "H";
-  }
-
-  EXPECT_FALSE(ends_with((query >> a).what(), "HHHHh"));
-  EXPECT_EQ((query >> b).what(), b);
-
-  EXPECT_EQ((query >> "Hello"sv).what(), "Hello");
-
-  auto strq = "Hi"sv;
-
-  EXPECT_EQ((query >> strq).what(), strq);
+  EXPECT_EQ(query >> IoError::__Reserved, "Unknown Error");
 }
 
 TEST(ReportTest, UnReportable) {
   struct Dummy {};
-  EXPECT_EQ((query >> Dummy{}).what(), "");
+  EXPECT_EQ(query >> Dummy{}, "");
   int c = 98;
   stx::Ref<int> g(c);
   EXPECT_EQ(g, c);
