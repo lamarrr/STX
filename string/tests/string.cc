24acdf85776c34bb5e2b7dd315fc381dde44b36d
Basit Ayantunde <rlamarrr@gmail.com>
2022-07-24 04:01:02 +0100

    updated tests

--- string/tests/string.cc
+++ string/tests/string.cc
@@ -10,5 +10,19 @@ TEST(StrTest, Init) {
 
   String a{stx::string::make_static("hello")};
   String b{stx::string::make(os_allocator, "waddup").unwrap()};
-  //
+
+  EXPECT_EQ(str[str.size()], '\0');
+
+  EXPECT_EQ(
+      stx::string::join(stx::os_allocator, " ", "Hello,", "Beautiful", "World!")
+          .unwrap(),
+      stx::string::make_static("Hello, Beautiful World!"));
+
+  EXPECT_EQ(
+      stx::string::join(stx::os_allocator, "???", "Hello,", "World!").unwrap(),
+      stx::string::make_static("Hello,???World!"));
+
+  EXPECT_EQ(stx::string::join(stx::os_allocator, "???", "", "", "").unwrap(),
+            stx::string::make_static("??????"));
+  // TODO(lamarrr): add more tests
 }
