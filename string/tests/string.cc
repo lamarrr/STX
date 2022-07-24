516b0b5222bdd06373f942b01aa62ea949e20b04
Basit Ayantunde <rlamarrr@gmail.com>
2022-07-24 05:51:00 +0100

    updated tests

--- string/tests/string.cc
+++ string/tests/string.cc
@@ -5,23 +5,47 @@
 using namespace stx;
 
 TEST(StrTest, Init) {
+  // TODO(lamarrr): add more tests
+
   String str;
 
-  String a{stx::string::make_static("hello")};
-  String b{stx::string::make(os_allocator, "waddup").unwrap()};
+  String a{string::make_static("hello")};
+  String b{string::make(os_allocator, "waddup").unwrap()};
 
   EXPECT_EQ(str[str.size()], '\0');
 
-  EXPECT_EQ(
-      stx::string::join(stx::os_allocator, " ", "Hello,", "Beautiful", "World!")
-          .unwrap(),
-      stx::string::make_static("Hello, Beautiful World!"));
+  EXPECT_EQ(a, "hello");
+  EXPECT_EQ(b, "waddup");
 
   EXPECT_EQ(
-      stx::string::join(stx::os_allocator, "???", "Hello,", "World!").unwrap(),
-      stx::string::make_static("Hello,???World!"));
+      string::join(os_allocator, " ", "Hello,", "Beautiful", "World!").unwrap(),
+      "Hello, Beautiful World!");
 
-  EXPECT_EQ(stx::string::join(stx::os_allocator, "???", "", "", "").unwrap(),
-            stx::string::make_static("??????"));
-  // TODO(lamarrr): add more tests
+  EXPECT_EQ(string::join(os_allocator, "???", "Hello,", "World!").unwrap(),
+            "Hello,???World!");
+
+  EXPECT_EQ(string::join(os_allocator, "???", "", "", "").unwrap(),
+            string::make_static("??????"));
+
+  EXPECT_EQ(string::join(os_allocator, "", "", "", "").unwrap(),
+            string::make_static(""));
+
+  {
+    std::vector<String> vec;
+    string::split("a,b,c,d,e,f", ",", [&](StringView token) {
+      vec.push_back(string::make(os_allocator, token).unwrap());
+    });
+  }
+
+  {
+    std::vector<String> vec;
+    string::split("a,b,c,d,e,f,", ",", [&](StringView token) {
+      vec.push_back(string::make(os_allocator, token).unwrap());
+    });
+  }
+
+  EXPECT_EQ(string::upper(os_allocator, "Hello, World!").unwrap(),
+            "HELLO, WORLD!");
+  EXPECT_EQ(string::lower(os_allocator, "hello, world!").unwrap(),
+            "hello, world!");
 }
