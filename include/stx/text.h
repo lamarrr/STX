6ceeb0153e2248e718cb6be72d4a7f3fb6dc5cc4
lamarrr <rlamarrr@gmail.com>
2022-12-28 23:20:17 +0000

    added text iterator

---
+++ include/stx/text.h
@@ -0,0 +1,73 @@
+#pragma once
+#include <cinttypes>
+#include <string_view>
+
+#include "stx/config.h"
+
+STX_BEGIN_NAMESPACE
+
+/// a utf-8 text iterator
+struct TextIterator {
+  uint8_t const* iter = nullptr;
+
+  TextIterator(char const* str) : iter{reinterpret_cast<uint8_t*>(str)} {}
+  TextIterator(std::string_view str) : TextIterator{str.data()} {}
+
+  uint32_t next() {
+    if ((*iter & 0xF8) == 0xF0) {
+      uint32_t c1 = *iter;
+      iter++;
+      uint32_t c2 = *iter;
+      iter++;
+      uint32_t c3 = *iter;
+      iter++;
+      uint32_t c4 = *iter;
+      iter++;
+      return c1 << 24 | c2 << 16 | c3 << 8 | c4;
+    } else if ((*iter & 0xF0) == 0xE0) {
+      uint32_t c1 = *iter;
+      iter++;
+      uint32_t c2 = *iter;
+      iter++;
+      uint32_t c3 = *iter;
+      iter++;
+      return c1 << 16 | c2 << 8 | c3;
+    } else if ((*iter & 0xE0) == 0xC0) {
+      uint32_t c1 = *iter;
+      iter++;
+      uint32_t c2 = *iter;
+      iter++;
+      return c1 << 8 | c2;
+    } else {
+      uint32_t c1 = *iter;
+      iter++;
+      return c1;
+    }
+  }
+};
+
+constexpr bool operator==(TextIterator const& a, uint8_t const* b) {
+  return a.iter == b;
+}
+
+constexpr bool operator!=(TextIterator const& a, uint8_t const* b) {
+  return a.iter != b;
+}
+
+constexpr bool operator==(uint8_t const* a, TextIterator const& b) {
+  return a == b.iter;
+}
+
+constexpr bool operator!=(uint8_t const* a, TextIterator const& b) {
+  return a != b.iter;
+}
+
+constexpr bool operator==(TextIterator const& a, TextIterator const& b) {
+  return a.iter == b.iter;
+}
+
+constexpr bool operator!=(TextIterator const& a, TextIterator const& b) {
+  return a.iter != b.iter;
+}
+
+STX_END_NAMESPACE
