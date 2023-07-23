eda9b400e69ebf94df4be879dcfcbeabc21ed06e
Basit Ayantunde <rlamarrr@gmail.com>
2023-07-24 00:00:26 +0100

    made utf8_next overload constexpr

--- include/stx/text.h
+++ include/stx/text.h
@@ -7,6 +7,7 @@
 STX_BEGIN_NAMESPACE
 
 /// gets the unicode codepoint at iter and then advances iter to the next codepoint
+///
 constexpr uint32_t utf8_next(uint8_t const *&iter)
 {
   if ((*iter & 0xF8) == 0xF0)
@@ -47,9 +48,44 @@ constexpr uint32_t utf8_next(uint8_t const *&iter)
   }
 }
 
-constexpr uint32_t utf8_next(char const *&iter)
+constexpr uint32_t utf8_next(int8_t const *&iter)
 {
-  return utf8_next(reinterpret_cast<uint8_t const *&>(iter));
+  if ((*iter & 0xF8) == 0xF0)
+  {
+    uint32_t c1 = (uint8_t) *iter;
+    iter++;
+    uint32_t c2 = (uint8_t) *iter;
+    iter++;
+    uint32_t c3 = (uint8_t) *iter;
+    iter++;
+    uint32_t c4 = (uint8_t) *iter;
+    iter++;
+    return c1 << 24 | c2 << 16 | c3 << 8 | c4;
+  }
+  else if ((*iter & 0xF0) == 0xE0)
+  {
+    uint32_t c1 = (uint8_t) *iter;
+    iter++;
+    uint32_t c2 = (uint8_t) *iter;
+    iter++;
+    uint32_t c3 = (uint8_t) *iter;
+    iter++;
+    return c1 << 16 | c2 << 8 | c3;
+  }
+  else if ((*iter & 0xE0) == 0xC0)
+  {
+    uint32_t c1 = (uint8_t) *iter;
+    iter++;
+    uint32_t c2 = (uint8_t) *iter;
+    iter++;
+    return c1 << 8 | c2;
+  }
+  else
+  {
+    uint32_t c1 = (uint8_t) *iter;
+    iter++;
+    return c1;
+  }
 }
 
 STX_END_NAMESPACE
