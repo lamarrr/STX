baf2498b6bba5adbb2b48a73d9d698e65c016c7a
Basit Ayantunde <rlamarrr@gmail.com>
2023-07-24 02:05:13 +0100

    removed utf8-next overload

--- include/stx/text.h
+++ include/stx/text.h
@@ -48,44 +48,4 @@ constexpr uint32_t utf8_next(uint8_t const *&iter)
   }
 }
 
-constexpr uint32_t utf8_next(int8_t const *&iter)
-{
-  if ((*iter & 0xF8) == 0xF0)
-  {
-    uint32_t c1 = (uint8_t) *iter;
-    iter++;
-    uint32_t c2 = (uint8_t) *iter;
-    iter++;
-    uint32_t c3 = (uint8_t) *iter;
-    iter++;
-    uint32_t c4 = (uint8_t) *iter;
-    iter++;
-    return c1 << 24 | c2 << 16 | c3 << 8 | c4;
-  }
-  else if ((*iter & 0xF0) == 0xE0)
-  {
-    uint32_t c1 = (uint8_t) *iter;
-    iter++;
-    uint32_t c2 = (uint8_t) *iter;
-    iter++;
-    uint32_t c3 = (uint8_t) *iter;
-    iter++;
-    return c1 << 16 | c2 << 8 | c3;
-  }
-  else if ((*iter & 0xE0) == 0xC0)
-  {
-    uint32_t c1 = (uint8_t) *iter;
-    iter++;
-    uint32_t c2 = (uint8_t) *iter;
-    iter++;
-    return c1 << 8 | c2;
-  }
-  else
-  {
-    uint32_t c1 = (uint8_t) *iter;
-    iter++;
-    return c1;
-  }
-}
-
 STX_END_NAMESPACE
